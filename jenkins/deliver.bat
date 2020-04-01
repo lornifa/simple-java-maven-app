906518e32667456815cd8c96457751f3fb8e8170
Florian <steffens-florian@web.de>
2020-04-01 11:25:35 +0200

    MODIFIED deliver.bat

--- jenkins/deliver.bat
+++ jenkins/deliver.bat
@@ -23,5 +23,7 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-
+echo test
 java -jar target/${NAME}-${VERSION}.jar
+
+echo finsihed
