2bf9e9fab114732e122d54fb85762fe3288874b2
Florian <steffens-florian@web.de>
2020-04-01 11:19:09 +0200

    MODIFIED: deliver.bat

--- jenkins/deliver.bat
+++ jenkins/deliver.bat
@@ -3,6 +3,9 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 
+cd ..
+echo worked
+
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 
